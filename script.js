8949919abd94a8bab0aeab1d9862796966d2585c
benirbenjamin <benirabok@gmail.com>
2023-09-24 22:08:25 +0200

    no changes

---
+++ script.js
@@ -0,0 +1,126 @@
+'use strict';
+//Declaring variables
+let currentPlayer=0
+const player0Name=document.getElementById('name--0')
+const player1Name=document.getElementById('name--1')
+
+let playerEl=document.querySelector('player--0')
+let playerEl1=document.querySelector('player--1')
+const btnRollDice=document.querySelector('.btn--roll')
+const currentScore0=document.getElementById('current--0')
+const currentScore1=document.getElementById('current--1')
+const message=document.querySelector('.dice-message')
+// const currentScore1=document.getElementById('current--1')
+const diceElement=document.querySelector('.dice')
+const btnHold=document.querySelector('.btn--hold')
+const btnNewGame=document.querySelector('.btn--new')
+const scoreEl0=document.getElementById('score--0')
+const scoreEl1=document.getElementById('score--1')
+// const score1Element=document.getElementById('score--1')
+const nameOf0Player=prompt("What is first palyer's name?")
+const nameOf1Player=prompt("What is Second Player's name")
+
+let currentScore=[0,0]
+let score=[0,0]
+let winner
+let playing=true
+
+player0Name.textContent=nameOf0Player
+player1Name.textContent=nameOf1Player
+scoreEl0.textContent=score[0]
+scoreEl1.textContent=score[1]
+
+let players=[nameOf0Player,nameOf1Player]
+//checking the winner
+//adding event listener to roll dice event
+
+if(playing){
+    
+const updtElements=function(dice){
+    diceElement.src=`dice-${dice}.png`
+    currentScore[currentPlayer]+=dice
+    document.getElementById(`current--${currentPlayer}`).textContent=currentScore[currentPlayer]
+ 
+
+}
+btnRollDice.addEventListener('click', function(){
+    const dice=Math.trunc(Math.random()*6)+1
+    console.log(dice);
+    
+    if(dice!==1){
+       updtElements(dice)
+    //    currentScore+=dice
+    
+}
+    else{
+        //switch to player 2
+        currentScore[currentPlayer]=0
+        document.getElementById(`current--${currentPlayer}`).textContent=currentScore[currentPlayer]
+        document.querySelector(`.player--${currentPlayer}`).classList.remove('player--active')
+        currentPlayer=currentPlayer===0?1:0
+        document.querySelector(`.player--${currentPlayer}`).classList.toggle('player--active')
+        // currentPlayer===0?currentScore0.textContent=currentScore:currentScore1.textContent=currentScore
+        // if(currentPlayer===0){
+        //     currentPlayer=1
+        //     document.querySelector('player--0)
+        // }else{
+        //     currentPlayer=0
+        //     playerEl.classList.toggle('player--active')
+        //     playerEl1.classList.toggle('player--active')
+        // } 
+    }
+
+
+})
+}
+
+//holding scores and switching players
+if(playing){
+btnHold.addEventListener('click', function(){
+
+score[`${currentPlayer}`]+=currentScore[currentPlayer]
+document.getElementById(`score--${currentPlayer}`).textContent=score[currentPlayer]
+currentScore[currentPlayer]=0
+document.getElementById(`current--${currentPlayer}`).textContent=currentScore[currentPlayer]
+
+document.querySelector(`.player--${currentPlayer}`).classList.remove('player--active')
+
+if(score[currentPlayer]>=300){
+    //console.log(`${score[currentPlayer]}>=20`);
+    playing=false
+    winner=players[currentPlayer]
+    message.classList.remove('hidden')
+    message.textContent=`⚠⚠⚠ Game over!!${winner} is the winner`
+    btnRollDice.setAttribute('disabled', '')
+    btnHold.setAttribute('disabled', '')
+   
+}
+else{
+    currentPlayer=currentPlayer===0?1:0
+
+document.querySelector(`.player--${currentPlayer}`).classList.toggle('player--active')
+}
+//checking the winner
+
+    //     if(currentPlayer===0){
+        // scoreEl0.textContent=score[currentPlayer]
+        //  currentScore0.textContent=currentScore
+//  currentPlayer=1
+//  playerEl1.classList.toggle('player--active')
+//  playerEl.classList.toggle('player--active')
+
+//     }
+//  else{scoreEl1.textContent=score[currentPlayer]
+
+//     currentScore1.textContent=currentScore
+//     currentPlayer=0
+//     playerEl.classList.toggle('player--active')
+//     playerEl1.classList.toggle('player--active')
+//  }
+
+})
+}
+btnNewGame.addEventListener('click', function(){
+
+    document.location.reload()
+})
